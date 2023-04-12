cf9601610292342da22e0573e9ffaaf1e19ab248
gaurav459433 <gaurav459433@gmail.com>
2023-04-13 01:44:45 +0530

    Welcome To Mood Analyser Program: Exception

---
+++ MoodAnalyzer.java
@@ -0,0 +1,7 @@
+package moodanalyzerproblem;
+
+public class MoodAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Mood Analyser Program: Exception");
+    }
+}
